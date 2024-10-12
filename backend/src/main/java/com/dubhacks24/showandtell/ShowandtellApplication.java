c52899e4f4e434c913d00c499dd1a824bf39b7b9
Jonah Kowal <jwbear@live.com>
2024-10-12 12:34:41 -0700

    initial commit

---
+++ backend/src/main/java/com/dubhacks24/showandtell/ShowandtellApplication.java
@@ -0,0 +1,13 @@
+package com.dubhacks24.showandtell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowandtellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowandtellApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dubhacks24/showandtell/ShowandtellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dubhacks24.showandtell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowandtellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
