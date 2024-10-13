5eae98bfc6c574e1a262dbcf3621c3d49c17afeb
Michelle Ge <michellege0161@gmail.com>
2024-10-12 23:14:19 -0700

    landing page updates

---
+++ frontend/my-app/src/landing/examples.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './card'
+
+const Examples = () => {
+  return (
+    <div className='w-full bg-[#b6d3f03b] py-24'>
+        <div className='max-w-[1280px] m-auto grid grid-cols-2'>
+            <Card />
+        </div>
+    </div>
+  )
+}
+
+export default Examples
--- frontend/my-app/src/landing/navbar.tsx
+++ frontend/my-app/src/landing/navbar.tsx
@@ -1,16 +1,15 @@
-
 import STlogo from '../img/STlogo.png';
 import React from 'react';
 
 const Navbar = () => {
   return (
     <div className='w-full h-[80px] bg-white border-b'>
-      <div className='max-w-[1480px] m-auto w-full h-full flex justify-between items-center'>
+      <div className='max-w-[1400px] m-auto w-full h-full flex justify-between items-center'>
 
-        <img src={STlogo} className="px-4 h-[70px]"/>
-        <div className="flex items-center">
-          <ul className="flex gap-5">
-            <li>Home</li>
+        <img src={STlogo} className="h-[70px]"/>
+        <div className="px-4 flex items-center">
+          <ul className="flex gap-4">
+            <li><a href="./landingpage">Home</a></li>
             <li><a href="./about">About</a></li>
             <li>Team</li>
             <li>Pricing</li>
@@ -18,8 +17,12 @@ const Navbar = () => {
         </div>
 
         <div className='flex'>
-          <button className='flex justify-between items-center bg-transparent px-6 gap-2'>Login</button>
-          <button className='m-6 px-8 py-3 rounded-md bg-satGreen text-white font-bold'>Sign Up</button>
+          <button className='px-6 gap-2 flex justify-between items-center rounded-md bg-transparent'>
+            Login
+          </button>
+          <button className='px-8 py-3 rounded-md bg-satGreen text-white font-bold'>
+            Sign Up
+          </button>
         </div>
 
       </div>
