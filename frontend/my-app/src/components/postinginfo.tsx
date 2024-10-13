ae92d1e24e75705ee397e64460043b564bab7042
Camaryn Garcia <camaryngarcia@gmail.com>
2024-10-12 22:58:22 -0700

    About and postinginfo pages edited

--- frontend/my-app/src/components/postinginfo.tsx
+++ frontend/my-app/src/components/postinginfo.tsx
@@ -9,14 +9,14 @@ export default function Postinginfo() {
             <div className="h-screen w-screen grid grid-cols-3 grid-rows-3 gap-3 justify-items-center outline">
                 <div></div>
                 <div>
-                    <h1 style={{textAlign:'center', fontSize:'25px', marginTop: '5rem'}}>Welcome to Show & Tell!<br></br>These are your days to share!</h1>
+                    <h1 style={{textAlign:'center', fontSize:'25px', marginTop: '7rem'}}>Welcome to Show & Tell!<br></br>These are your days to share!</h1>
                 </div>
                 <div></div>
                 <div>
                     <img src={sBlock} alt="Show And Tell S Block" width="320" className=""></img>
                 </div>
                 <div className="outline w-full">
-                    <h1 style={{textAlign: 'center', fontSize: '18px'}}>10/13<br></br><br></br>10/31</h1>
+                    <h1 style={{textAlign: 'center', fontSize: '18px'}}><a href="./group">Group 1</a>: 10/13<br></br><br></br><a href="./group">Group 2</a>: 10/21</h1>
                 </div>
                 <div>
                     <img src={tBlock} alt="Show And Tell T Block" width="350" className=""></img>
