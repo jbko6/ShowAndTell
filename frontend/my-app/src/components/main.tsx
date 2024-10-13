dde2802899db4fc8c74bd573768c11497cb2d268
Aiden Sparks <nedia.sparks@gmail.com>
2024-10-12 17:15:47 -0700

    Updated Group,Home, added colors

--- frontend/my-app/src/components/main.tsx
+++ frontend/my-app/src/components/main.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Home from './home';
-import Test from './test';
+import Group from './group';
 
 const Main = () => {
     return(
         <Router>
             <Routes>
                 <Route path='/' element={<Home/>}></Route>
-                <Route path='/test' element={<Test/>}></Route>
+                <Route path='/group' element={<Group/>}></Route>
             </Routes>
         </Router>
     );
--- frontend/my-app/tailwind.config.js
+++ frontend/my-app/tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      satGreen: '#649b50',
+      satBlue: '#359dcf',
+      satRed: '#ea5a60'
+    },
     extend: {},
   },
   plugins: [],
