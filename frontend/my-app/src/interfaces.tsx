76ea0e2d23a3bab4bf5433dd164da0d2f4544a74
Jonah Kowal <jwbear@live.com>
2024-10-13 02:46:10 -0700

    data based post viewing

--- frontend/my-app/src/interfaces.tsx
+++ frontend/my-app/src/interfaces.tsx
@@ -1,9 +1,30 @@
 import {ObjectId} from 'bson';
 
-export type User = {
+export type UserData = {
     sub: string
     name: string
     nickname: string
     email: string
     picture: string
+}
+
+export type GroupData = {
+    id: ObjectId,
+    categoryName: string,
+    member: Array<string>,
+    posts: Array<PostData>,
+}
+
+export type PostData = {
+    id: ObjectId
+    title: string
+    markdown: string
+    author: string
+    comments: Array<Comment>
+}
+
+export type CommentData = {
+    id: ObjectId
+    author: string
+    content: string
 }
